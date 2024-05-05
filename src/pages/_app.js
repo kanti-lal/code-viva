648d0e2e2db6a7c1f8be9ef8a77b48cb008e49b2
kanti-lal <kanti.ylocation@gmail.com>
2024-05-06 01:39:14 +0530

    feat: mui and reactquery setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+
+  // return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Provide the client to our App */}
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  );
 }
